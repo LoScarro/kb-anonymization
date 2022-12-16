f327833e3d934cde42237ea20dca3a15f75fd065
LoScarro <davisca2000@gmail.com>
2022-12-16 16:39:09 +0100

    created test for evaluating path condition

--- src/program_execution.py
+++ src/program_execution.py
@@ -1,4 +1,5 @@
 import logging
+from tests import test as test
 
 # Initialize PC_Buckets, a dictionary which groups tuples based on path conditions, mapping PC to a list of tuples which have that PC
 def program_execution(R, k):
@@ -6,8 +7,15 @@ def program_execution(R, k):
 
     # FILL PC_Buckets
     
-    for t in R.iterrows():
+    for t in R.itertuples():
         print(t.age)
+        pc = []
+        test.P_test(t, pc)
+
+        print(pc)
+
+
+
     #   pc = execute P with t and collect the path condition 
     #   If PC_Buckets does not contains pc
     #     PC_Buckets[pc] = []
