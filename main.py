5025ca722aee6e65a33d7e411bbe0db01dc79340
LLaCorte <lorenzolacorte99@gmail.com>
2022-12-26 12:25:02 +0100

    k-anon not working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 from src import program_execution as pe
+from src import k_anonymization as ka
 import numpy as np
 import pandas as pd
 import logging
@@ -10,6 +11,7 @@ def main():
     parser.add_argument('--output_file', type=str, default="data/out.csv")
     parser.add_argument('--k', type=int, default=2)
     parser.add_argument('--bpl', type=str, default="PT", help="Behaviour Preservation Level (PT) (PF) (IT)")
+    parser.add_argument('--sensitive_columns', type=str, default="disease")
     parser.add_argument("--verbose", action='store_true')
 
     args = parser.parse_args()
@@ -19,8 +21,14 @@ def main():
     
     R = pd.read_csv(args.input_file)
 
+    sensitive = set(args.sensitive_columns.split())
+    all_cols = set(R.columns.values.tolist())
+    non_sensitive = all_cols - sensitive
+
     PC_Buckets = pe.program_execution(R, args.k)
-    print(PC_Buckets)
+    logging.info("Buckets Created:" + str(PC_Buckets))
+
+    A = ka.k_anonymization(PC_Buckets, sensitive, non_sensitive, args.k, args.bpl)
 
     return
 
--- src/k_anonymization.py
+++ src/k_anonymization.py
@@ -1,12 +1,32 @@
-# Initialize A, a dataset holding intermediate k-anonymized groups
-# 
-# For each Bucket with (key, value) = (pc, B)
-#   B' = Invoke a k-anonymization algorithm on B and get its result 
-#   remove duplicates in B'
-# 
-#   For each tuple b ∈ B'
-#     If O == "I-T":
-#       If len(b) <= 1 or no field in b contain concrete values
-#         Output “Error: unsatisfiable case”
-#         continue
-#     A.add({b, pc, B})
+import logging
+import anonypy
+import pandas as pd
+
+def is_concrete(field):
+    return True
+
+
+# Initialize PC_Buckets, a dictionary which groups tuples based on path conditions, mapping PC to a list of tuples which have that PC
+def k_anonymization(PC_Buckets, sd, qi, k, bpl):
+    logging.info("k-anonymization Module Starting")
+
+    # Initialize A, a dataset holding intermediate k-anonymized groups
+    A = set()
+    # For each Bucket with (key, value) = (pc, B)
+    for pc, B in PC_Buckets.items():
+        # Invoke a k-anonymization algorithm on B and get its result 
+        p = anonypy.Preserver(B, qi, sd)
+        B_anon = p.anonymize_k_anonymity(k)
+        #   remove duplicates in B'
+        # remove_duplicates(B_anon)
+    
+    
+        for b in B_anon:
+            # no field in b contain concrete values
+            if bpl == "IT" and (len(b) <= 1 or not any(is_concrete(field) for field in b)):
+                logging.error("Error: unsatisfiable case")
+                continue
+            
+            A.add((b, pc, B))
+
+    return A
