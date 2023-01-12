ce74651dbd46e2bf3fab0363ffabd4bc0811cb8d
LoScarro <davisca2000@gmail.com>
2023-01-12 14:21:30 +0100

    implemented test cases

--- path/test.py
+++ path/test.py
@@ -2,7 +2,6 @@ def P_test(t, pc):
     test_1(t, pc)
     test_2(t, pc)
     test_3(t, pc)
-    test_4(t, pc)
 
 ##########
 # TEST 1 #
@@ -25,8 +24,6 @@ def test_1(t, pc):
     else:
         pc += [('pay', '<=', 160000)]
 
-    
-    
 def test_2(t, pc):
     if t.job == "Special Nurse" or t.job == "SPECIAL NURSE":
         pc += [('job', '==', "Special Nurse")]
@@ -34,10 +31,25 @@ def test_2(t, pc):
         pc += [('job', '!=', "Special Nurse")]
     
     if t.zip_code <= 20000:
-            pc += [('zip_code', '<=', 20000)]
+        pc += [('zip_code', '<=', 20000)]
         
-    elif t.zip_code <= 40000:
+    elif 20000 < t.zip_code <= 40000:
         pc += [('zip_code', '<=', 40000)]
-        pc += [('zip_code', '>=', 20000)]
+        pc += [('zip_code', '>', 20000)]
     else:
-        pc += [('zip_code', '>=', 40000)]
+        pc += [('zip_code', '>', 40000)]
+
+def test_3(t, pc):
+    if 160000 <= t.pay <= 240000 and t.zip_code <= 30000:
+        pc += [('pay', '<=', 160000)]
+        pc += [('pay', '>', 240000)]
+        pc += [('zip_code', '<=', 30000)]
+    
+    elif 160000 <= t.pay <= 320000 and 30000 < t.zip_code <= 40000:
+        pc += [('pay', '<=', 320000)]
+        pc += [('pay', '>', 160000)]
+        pc += [('zip_code', '<=', 40000)]
+        pc += [('zip_code', '>', 30000)]
+    else:
+        pc += [('pay', '<=', 160000)]
+        pc += [('zip_code', '>', 40000)]
