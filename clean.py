08acbacb39d75ffabb7a43b51c9200d1903687e6
LLaCorte <lorenzolacorte99@gmail.com>
2023-01-13 17:34:55 +0100

    clean.py

--- clean.py
+++ clean.py
@@ -1,7 +1,15 @@
+# script used for generating dbs
+
 import numpy as np
 import pandas as pd
+import argparse
 
-df = pd.read_csv("data/db_50000.csv", low_memory=False)
-df = df.sample(frac = 1)
-df = df.iloc[:100]
-df.to_csv("data/db_100.csv", index = False, header=True)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rows', type=str, default=20)
+    args = parser.parse_args()
+
+    df = pd.read_csv("data/db_50000.csv", low_memory=False)
+    df = df.sample(frac = 1)
+    df = df.iloc[:int(args.rows)]
+    df.to_csv(f"data/db_{args.rows}.csv", index = False, header=True)
